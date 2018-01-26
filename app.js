81b3aa91a456dfdeda1e0ac2a10777db31bbe014
DAS-96 <damiano.sartori@studenti.unitn.it>
2018-01-26 11:26:50 +0100

    Add function to perform a fetch

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require('express'),
     bodyParser = require('body-parser');
 
+var check = require('./checker.js');
+
 const app = express();
 app.use(bodyParser.json());
 
@@ -11,6 +13,11 @@ app.set('port', (process.env.PORT || 5000));
 // a useless function that returns a fixed object. you can use it, if you want, for testing purposes
 app.get('/count',function (req, res) {
     res.json({count: 5})
+})
+
+app.get('/check', function (req, res) {
+  check("http://localhost:5000/count", {lato1: 3, lato2: 5}, {area: 15}, 200);
+  res.send("OK");
 })
 
 app.listen(app.get('port'), function() {
--- checker.js
+++ checker.js
@@ -3,13 +3,56 @@ const fetch = require('node-fetch')
 
 function check(url, invocationParameters,  expectedResultData, expectedResultStatus) {
 
-    const checkResult = { // this is the object you need to set and return
+  var paramNames = Object.keys(invocationParameters);
+  console.log(paramNames);
+  var paramValues = Object.values(invocationParameters);
+  console.log(paramValues);
+
+  var completeUrl = url + "?";
+
+  for (i = 0; i < paramNames.length; i++){
+    completeUrl = completeUrl + paramNames[i] + "=" + paramValues[i];
+    if (i < (paramNames.length - 1)){
+      completeUrl = completeUrl + "&";
+    }
+  }
+
+  console.log(completeUrl);
+
+  fetch(completeUrl, {headers: 'Accept: application/json'})
+    .then(function(res){
+      console.log("FACENDO LA GET");
+      console.log(res.status);
+      var json = JSON.parse(res);
+      //var json = JSON.parse(res.body);
+      console.log(res);
+
+      var compare = compareResults(expectedResultData, {area: 15});
+
+      const checkResult = { // this is the object you need to set and return
+          urlChecked: completeUrl,
+          resultData: null,
+          resultStatus: res.status,
+          statusTestPassed: (res.status == expectedResultStatus),
+          resultDataAsExpected: compare
+      };
+
+      console.log(checkResult);
+
+    })
+    .catch(function(err){
+      console.log("ERRORE" + err);
+    });
+
+
+
+    /*const checkResult = { // this is the object you need to set and return
         urlChecked: url,
         resultData: null,
         resultStatus: null,
         statusTestPassed: null,
         resultDataAsExpected: null
-    }
+    }*/
 
 
 
